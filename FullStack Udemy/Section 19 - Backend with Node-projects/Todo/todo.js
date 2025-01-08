ef0401c58cbc8cbaed10c0481bc5dbad49cddf7b
pradeepSharmadev <amarahipradeep7319@gmail.com>
2025-01-08 15:08:52 +0530

    File Hanling with Node.js Module fs is working and add task is complete

---
+++ FullStack Udemy/Section 19 - Backend with Node-projects/Todo/todo.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const filePath = "./tasks.json";
+
+//functions to handle file
+// load or read the existing file
+const loadTasks = () => {
+  // bound with error handling
+  try {
+    const dataBuffer = fs.readFileSync(filePath);
+    const dataJson = dataBuffer.toString();
+    const regularJson = JSON.parse(dataJson);
+    return regularJson; // reading successfully binary file or like bit fil
+  } catch (error) {
+    // error then return empty task
+    return [];
+  }
+};
+
+// saveTask or save file
+const saveTask = (tasks) => {
+    // write file
+    const dataJson = JSON.stringify(tasks)
+    fs.writeFileSync(filePath, dataJson)
+};
+
+//add Task to file
+const addTask = (task) => {
+  const tasks = loadTasks(); //we have assess of simple object do operation what needed
+  //tasks.push(tasks); this error is called converting circular structure to JSON //Error must fix it by tasks.push(task)
+  tasks.push({task}); // [{ task: "go to gym" }];
+  saveTask(tasks);
+  console.log("Task is Added successfully", task)
+};
+
+//access command and argument
+const command = process.argv[2];
+const argument = process.argv[3];
+
+//command based query
+if (command === "add") {
+  addTask(argument);
+} else if (command === "list") {
+  listTasks();
+} else if (command === "remove") {
+  removeTask(parseInt(argument));
+} else {
+  console.log("Command Not Found!");
+}
+
+
+// ADD Task command : node todo.js add "go to gym"
